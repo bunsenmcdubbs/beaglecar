df3585d0c8611a70b7e41f54733627a692d9c1db
Andrew Dai <theman@andrewdai.co>
2013-12-27 23:47:07 -0500

    Better test code - getting both most and least significant bytes and printing. Need to combine the 2 bytes into a 16 bit word and then into G's
    	modified:   src/test/test.py

--- src/test/test.py
+++ src/test/test.py
@@ -1,6 +1,12 @@
 from Adafruit_I2C import Adafruit_I2C
- 
+from time import sleep
+
 i2c = Adafruit_I2C(0x68)
 
 i2c.write16(0x6B, 0)
-print(i2c.readS8(0x3B))
+
+for x in range(0, 20):
+	b = i2c.readS8(0x3b)
+	s = i2c.readU8(0x3c)
+	print (str(b) + " " + str(s))
+	sleep(0.2)
