7735e26267a19fd325829ff1916f9cf5ea068f5a
Andrew Dai <theman@andrewdai.co>
2013-12-28 23:36:49 -0500

    Cleaned up test code
    	modified:   src/test/test.py

--- src/test/test.py
+++ src/test/test.py
@@ -7,20 +7,22 @@ i2c = Adafruit_I2C(0x68)
 
 # wake up the device (out of sleep mode)
 # bit 6 on register 0x6B set to 0
-i2c.write8(0x6B, 0)
+i2c.write16(0x6B, 0)
+
+print("X axis accelerations (in g's)")
 
 # read and print acceleration on x axis
 # Most significant byte on 0x3b
 # Least significant byte on 0x3c
 # Combined to obtain raw acceleration data
-for x in range(0, 20):
+for x in range(0, 5):
 	# getting values from the registers
 	b = i2c.readS8(0x3b)
 	s = i2c.readU8(0x3c)
-	print (str(b) + " " + str(s))
 	# converting 2 8 bit words into a 16 bit
 	# signed "raw" value
-	raw = b * 255 + s
-	print (raw)
+	raw = b * 256 + s
 	# still needs to be converted into G-forces
+	g = raw / 16384.
+	print (str(g))
 	sleep(0.2)
