8c64ebbdeef61e63105cd946bff139f32766d74f
Andrew Dai <theman@andrewdai.co>
2014-02-07 10:35:04 -0500

    Commenting code

--- src/pos.py
+++ src/pos.py
@@ -3,22 +3,35 @@ import rospy
 from std_msgs.msg import Float32
 import time
 
+# This script receives turn rates from the "chatter" topic
+# and integrates to find a current heading relative to the
+# initial heading
+
+# function to return current time in milliseconds
 current_milli_time = lambda: int(round(time.time() * 1000))
 
-angle = 0
-last_time = 0
+# global variables
+angle = 0 # angular position relative to start heading
+last_time = 0 # time in milliseconds at the last integration
 
+# receives data from topic and integrates rates to calculate 
+# current heading
+# TODO: timestamps
 def callback(data):
     global angle
     global last_time
+    # gets the current time and calculates the seconds elapsed
+    # since the last integration
     curr_time = current_milli_time()
     t_elapsed = (curr_time - last_time) / 1000.
     last_time = curr_time
-    print(str(t_elapsed))
+    # print(str(t_elapsed))
+    # integration!
     angle = angle + data.data * t_elapsed
+    # logging current heading
     rospy.loginfo(rospy.get_name() + "\n" + str(angle))
 
-
+# ROS Code
 def listener():
     rospy.init_node('listener', anonymous=True)
     rospy.Subscriber("chatter", Float32, callback)
