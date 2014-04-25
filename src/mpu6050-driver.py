c627de8d725706d9a58f0ab6c949793e7e492a2c
Andrew Dai <theman@andrewdai.co>
2014-04-24 23:49:31 -0400

    moved around some stuff.... really sketchy progress on mpu6050 driver

---
+++ src/mpu6050-driver.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+import rospy
+import time
+from Adafruit_I2C import Adafruit_I2C
+from sensor_msgs.msg import Imu
+from std_msgs.msg import Header, Float64, Time
+from geometry_msgs.msg import Vector3
+
+# x, y, z in MSB, LSB order
+accel = ((0x3b, 0x3c), (0x3d, 0x3e), (0x3f, 0x40))
+gyro = ((0x43, 0x44), (0x45, 0x46), (0x47, 0x48))
+# MSB, LSB
+temp = (0x41, 0x42)
+
+current_milli_time = lambda: int(round(time.time() * 1000))
+
+def read (imu, addr, scale = 1.0, bias = 0.0):
+  "Reads data from addresses specified [MSB, LSB] and returns a decimal"
+  raw = imu.readList(addr, 2)
+  combined = raw[0] * 256 + raw[1]
+  return combined / scale - bias
+
+def calibrate (imu, addrs, samples = 100, freq = 10.):
+  "Averages a number of samples taken at a certain frequency over addresses"
+  global zeros
+  zeros = [0] * len(addrs)
+
+  for sample in range (0, samples):
+    for i in range(0, len(addrs)):
+      addr = addrs[i]
+      zeros[i] += read(imu, addr)
+    time.sleep(1/freq)
+
+  for total in zeros:
+    total /= samples
+
+  return zeros
+
+def make_header():
+  global seq
+  header = Header()
+  header.seq = seq
+  seq += 1
+  time = Time()
+  time.time = current_milli_time();
+  header.stamp = time
+  header.frame_id = "0"
+  return header
+
+def read_gyros():
+  return read_mult(gyro)
+
+def read_accels():
+  return read_mult(accel)
+
+def read_mult(addrs, error_index = 0):
+  result = Vector3()
+  vals = []
+  for i in range(len(addrs)):
+    vals.append(read(imu, addrs[i], bias = zeros[error_index+i]))
+  result.x, result.y, result.z = vals
+  return result
+
+def talker():
+
+  global pub
+  pub = rospy.Publisher('mpu6050', Imu)
+  rospy.init_node('Joy2Turtle')
+
+  global seq
+  seq = 0
+  while not rospy.is_shutdown():
+    sample = Imu()
+    sample.header = make_header()
+    sample.orientation_covariance[0] = -1
+    sample.angular_velocity_covariance = [0]*9
+    sample.angular_velocity = read_gypos()
+    sample.linear_acceleartion_covariance = [0]*9
+    sample.linear_acceleration = read_accels()
+    pub.publish(sample)
+    time.sleep(0.001)
+
+if __name__ = "__main__":
+  try:
+      global imu
+      imu = Adafruit_I2C(0x68) # initializing the i2c device
+      imu.write8(0x6b, 0) # waking the imu
+      talker() # ROS Stuff
+  except rospy.ROSInterruptException:
+      pass
---
+++ src/servo_controller.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+import Adafruit_BBIO.PWM as PWM
+
+# Author: Andrew Dai
+# MIT License
+
+# This class controls a servo by converting angles between 0 and 180
+# into the proper frequency for servo PWM. It can also have maximum
+# and minimum angles that clamp input angles.
+
+class Servo:
+    """Servo Controller defaults to pin8_13"""
+
+    # Starts the servo controller with a PWM pin and initial angle.
+    # Todo: set a GPIO pin to control on/off
+    def __init__(self, pin = "P8_13", start_angle = 90):
+        self.servo_pin = pin
+        self.duty_max = 14.5
+        self.duty_min = 3.
+        self.duty_span = self.duty_max - self.duty_min
+        self.upper_lim = 180.
+        self.lower_lim = 0.
+        self.running = False
+        self.angle = 0.
+        self.set_angle(start_angle)
+        return
+
+    # Still need to implement powering on
+    def start(self):
+        # Step 1: turn on 5v power line (will need to know GPIO pin)
+        print "Starting the servo! Turn on the 5v line"
+        # Step 2: start PWM
+        PWM.start(self.servo_pin, self.angle, 60.0)
+        # Step 3: set running flag to true
+        self.running = True
+        return
+
+    # Still need to implement powering off
+    def stop(self):
+        print "Stopping the servo! Turn off the 5v line"
+        PWM.stop(self.servo_pin)
+        PWM.cleanup()
+        self.running = False
+        return
+
+    # Prevents the angle from going beyond the upper and lower angle limits
+    def clamp (self, angle):
+        if angle < self.lower_lim: return self.lower_lim
+        if angle > self.upper_lim: return self.upper_lim
+        return angle
+
+    # Sets the angle of the servo (clamped).
+    # If the servo is not currently on (self.running == True) then only the
+    # self.angle variable is changed. Otherwise the PWM duty cycle is also
+    # changed.
+    def set_angle(self, angle, clamp = True):
+        if clamp: self.angle = float(self.clamp(angle))
+        else: self.angle = float(angle)
+        if self.running:
+            duty = self.angle / 180. * self.duty_span + self.duty_min
+            PWM.set_duty_cycle(self.servo_pin, duty)
+        # print str(self.angle)
+        return
+
+def main():
+    print "main method"
+    servo = Servo()
+    while True:
+        input = raw_input("Enter and angle from 0 to 180. 'x' to exit:")
+        if input == 'x':
+            servo.stop()
+            break
+        servo.set_angle(input)
+
+if __name__ == '__main__': main()
