3d730d78dbdff63f9fd8992b31e4ef2f51f2a080
Andrew Dai <theman@andrewdai.co>
2014-05-12 10:42:30 -0400

    added documentation and start stop methods

--- src/drive.py
+++ src/drive.py
@@ -6,18 +6,26 @@ import Adafruit_BBIO.PWM as PWM
 
 class Car:
   def __init__(self, s_pin = "P8_13", f_pin = "P9_14", b_pin = "P9_22", debug = false):
+    # initialize servo for steering
     self.steer = Servo(s_pin)
+    # save PWM pins for forward and reverse
     self.forward = f_pin
     self.reverse = b_pin
-    self.steer.start()
-    init_servo()
+    # start servos and motors
+    start()
+    # center the steering
+    center_steering()
 
+  # set the "steering wheel" of the car
+  # negative is left
+  # positive is right
   def turn(self, angle = 0):
     self.steer.set_angle(self.center + angle)
     if debug:
-      print center + angle
+      print angle
     return
 
+  # set the car speed
   def set_speed(self, speed):
     speed = clamp(speed, -100, 100)
     if speed > 0:
@@ -28,7 +36,23 @@ class Car:
         PWM.set_duty_cycle(reverse, -speed)
     return
 
-  def init_servo():
+  # start PWM lines for forward and reverse and start steering servo
+  def start(self):
+    PWM.start(self.forward, 0, 200)
+    PWM.start(self.reverse, 0, 200)
+    self.steer.start()
+    return
+
+  # stop PWM lines for forward and reverse and stop steering servo
+  def stop(self):
+    PWM.stop(self.forward)
+    PWM.stop(self.reverse)
+    self.steer.stop()
+    PWM.cleanup()
+    return
+
+  # center the steering servo between two limit switches
+  def center_steering():
     pin1 = "P8_12"
     pin2 = "P8_14"
     GPIO.setup(pin1, GPIO.IN)
@@ -64,6 +88,7 @@ class Car:
 
     # calculating center from left and right limits
     self.center = (limit1 + limit2) / 2
+    # calculate total range (102% of limit differences)
     self.span = (limit1 - limit2) / 2 * 1.02 # allow for a little extra wiggle
     self.steer.set_limits(limit2, limit1)
 
@@ -73,6 +98,7 @@ class Car:
     servo.set_angle(center)
     return
 
+  # clamp x value between specified min and max values
   def clamp(x, min, max):
     if min > max:
       return False
